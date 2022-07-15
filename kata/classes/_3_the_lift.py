5b5ed283f01e4ec95a52c8eafc0a59d2aecfc7a7
Kaleb Sofer <kalebsofer@gmail.com>
2022-07-15 10:06:57 +0100

    completed lift kata

--- kata/classes/_3_the_lift.py
+++ kata/classes/_3_the_lift.py
@@ -29,65 +29,61 @@ for floor,queue in dict
 
 '''
 
-
 class Dinglemouse(object):
 
     def __init__(self, queues, capacity):
         self.queues = [list(x) for x in queues]
         self.capacity = capacity
-        self.lift = []
+        self.elevator = []
         self.people_waiting = True
 
-    def _personLeaves(self, floor, stop=False):
-        for person in self.lift:
-                    if person == floor:
-                        self.lift.remove(person)
-                        stop = True
-        return stop
-
-    def _handleQueue(self, person, floor, stop=True):
-        if self.capacity > len(self.lift):
-            self.lift.append(person)
-            self.queues[floor].remove(person)
-        else:
-            self.people_waiting = True
-        return stop
-
-    def _appendResult(self, stop, res, floor):
-        if stop and not res[-1] == floor:
-            res.append(floor)
-
-
-    def _goUp(self, res):
-        for floor in range(0, len(self.queues)):
-                stop = self._personLeaves(floor, stop=False)
-                for person in self.queues[floor]:
-                    if person > floor:
-                        stop = self._handleQueue(person, floor, stop=True)
-                self._appendResult(stop, res, floor)
-        return
-    
-    def _goDown(self, res):
-        for floor in range(len(self.queues) -1, -1, -1):
-                stop = self._personLeaves(floor, stop=False)
-                for person in self.queues[floor]:
-                    if person < floor:
-                        stop = self._handleQueue(person, floor, stop=True)
-                self._appendResult(stop, res, floor)
-        return
-
-
     def theLift(self):
+
         res = [0]
-        
+
         while self.people_waiting:
             self.people_waiting = False
-            self._goUp(res)
-            self._goDown(res)
-            
+
+            res = self._run_lift(res, 'up')
+            res = self._run_lift(res, 'down')
+
         if res[-1] != 0:
             res.append(0)
 
         return res
-    
 
+    def _run_lift(self, stops, direction):
+        f_order = range(len(self.queues) -1, -1, -1) if direction == 'down' else range(0, len(self.queues))
+        for floor in f_order:
+            exits = self._people_exit(floor)
+            enters = self._people_enter(floor, direction)
+            if exits or enters and not stops[-1] == floor:
+                stops.append(floor)
+        return stops
+
+    def _people_exit(self, floor):
+        stop = False
+
+        for person in self.elevator[:]:
+            if person == floor:
+                self.elevator.remove(person)
+                stop = True
+
+        return stop
+
+    def _people_enter(self, floor, direction):
+        stop = False
+
+        for person in self.queues[floor][:]:
+            person_enters = person < floor if direction == 'down' else person > floor
+
+            if person_enters:
+                stop = True
+
+                if self.capacity > len(self.elevator):
+                    self.elevator.append(person)
+                    self.queues[floor].remove(person)
+                else:
+                    self.people_waiting = True
+
+        return stop
--- tests/test_the-lift.py
+++ tests/test_the-lift.py
@@ -2,16 +2,17 @@ import pytest
 from kata.classes._3_the_lift import Dinglemouse
 
 testdata = [
-         (( (),(),(5,5,5),(),(),(),()), [0, 2, 5, 0]),
-         (( (),(),(1,1),(),(),(),()), [0, 2, 1, 0]),
-         (( (),(3,),(4,),(),(5,),(),()),[0, 1, 2, 3, 4, 5, 0]),
-         (( (),(0,),(),(),(2,),(3,),()), [0, 5, 4, 3, 2, 1, 0])
+         (( (),(),(5,5,5),(),(),(),()), 5, [0, 2, 5, 0]),
+         (( (),(),(1,1),(),(),(),()), 5, [0, 2, 1, 0]),
+         (( (),(3,),(4,),(),(5,),(),()), 5, [0, 1, 2, 3, 4, 5, 0]),
+         (( (),(0,),(),(),(2,),(3,),()), 5, [0, 5, 4, 3, 2, 1, 0]),
+         (( (),(),(4,4,4,4),(),(2,2,2,2),(),()), 2, [0, 2, 4, 2, 4, 2, 0]) # [0, 2, 4, 2, 0]
             ]
 
   
-@pytest.mark.parametrize(('queues', 'expected_result'), testdata)
-def test_lift(queues, expected_result):
-    lift = Dinglemouse(queues, 5)
+@pytest.mark.parametrize(('queues', 'capacity', 'expected_result'), testdata)
+def test_lift(queues, capacity, expected_result):
+    lift = Dinglemouse(queues, capacity)
     assert lift.theLift() == expected_result
 
 
